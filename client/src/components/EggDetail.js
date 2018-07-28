a12be3d3d4ad8099301f70dc0ec27ab95818bae9
cynthialu42 <clu2334@gmail.com>
2018-07-27 21:15:52 -0500

    aviary complete

--- client/src/components/EggDetail.js
+++ client/src/components/EggDetail.js
@@ -1,14 +1,97 @@
 import React from 'react';
 import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
-const EggDetail = ({ egg }) => {
+const EggDetail = ({ habit }) => {
+    const { complete, _id, name, iteration, count, description, egg, date } = habit;
+    const { 
+        headerContentStyle, 
+        thumbnailStyle, 
+        thumbnailContainerStyle, 
+        titleStyle,
+        imageStyle,
+        addButtonStyle,
+        addTextStyle,
+        listStyle,
+        itemStyle,
+        birdDescription
+    } = styles;
     return (
-        <View>
-            <Text>{egg.name}</Text>
-            <Text>{egg.bird_description}</Text>
-        </View>
+        <Card>
+            <CardSection>
+                <View style={thumbnailContainerStyle}>
+                    {egg.is_hatched ?
+                        <Image style={itemStyle} source={{uri: egg.end_img}} />
+                        :
+                        <Image style={itemStyle} source={{uri: egg.start_img}} />
+                    }
+
+                </View>
+                <View style={headerContentStyle}>
+                    {egg.is_hatched ?
+                        (<View style={headerContentStyle}><Text style={titleStyle}>{egg.name}</Text>
+                            <Text style={birdDescription}>"{egg.bird_description}"</Text>
+                            <Text style={{fontSize: 12}}>Hatched from "{name}" habit</Text></View>)
+                        :
+                        (<View style={headerContentStyle}><Text style={titleStyle}>???</Text>
+                            <Text style={birdDescription}>Egg has not hatched yet!</Text>
+                            <Text style={{fontSize: 12}}>Hatching from "{name}" habit</Text></View>)
+                    }
+                    
+                </View>
+            </CardSection>
+        </Card>
         
     )
 };
+
+const styles = {
+    listStyle: {
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    },
+    itemStyle: {
+        margin: 3,
+        width: 150,
+        height: 150
+    },
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        flex: 1
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    titleStyle: {
+        fontSize: 18
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    },
+    addButtonStyle:{
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        paddingLeft: 10,
+        paddingRight: 10
+    },
+    addTextStyle:{
+        fontSize: 30,
+        color: '#37a69a'
+    },
+    birdDescription: {
+        fontStyle: 'italic'
+    }
+}
 
 export default EggDetail;
--- client/src/components/EggList.js
+++ client/src/components/EggList.js
@@ -2,27 +2,38 @@ import React, { Component } from 'react';
 import { ScrollView, Text } from 'react-native';
 import API from './../utils/API';
 import EggDetail from './EggDetail';
+import Header from './Header';
 
 class EggList extends Component {
     state = {
-        eggs: []
+        habits: []
     }
     componentDidMount() {
-        API.getEggs().then(resData => this.setState({ eggs: resData }));
-        // fetch('http://localhost:3001/api/eggs')
-        // .then(res => res.json())
-        // .then(resData => this.setState({ eggs: resData }));
+        this.didFocusListener = this.props.navigation.addListener(
+            'didFocus',
+            () => {this.loadHabits()},
+        );
+        
+    }
+    componentWillUnmount() {
+        this.didFocusListener.remove();
     }
 
+    loadHabits = () => {
+        API.getHabits()
+            .then(resData => this.setState({ habits: resData }))
+            .catch(err => console.log(err))
+    }
     renderEggs() {
-        return this.state.eggs.map(egg => {
-            return <EggDetail key={egg.egg_id} egg={egg} />
+        return this.state.habits.map(habit => {
+            return <EggDetail key={habit._id} habit={habit} />
         })
     }
     render() {
         console.log(this.state);
         return (
             <ScrollView>
+                <Header headerText={'The Aviary'} />
                 {this.renderEggs()}
             </ScrollView>
         )
